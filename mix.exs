3704d98b199c19273bb7477246f10e4a9e6ee36d
Sean Lang <slang800@gmail.com>
2019-01-24 15:27:20 -0600

    add mix format config & run formatter

--- mix.exs
+++ mix.exs
@@ -2,12 +2,14 @@ defmodule Bencodex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :bencodex,
-     version: "1.0.0",
-     elixir: "~> 1.0",
-     description: "Encoder and decoder for the bencode format",
-     package: package(),
-     deps: deps()]
+    [
+      app: :bencodex,
+      version: "1.0.0",
+      elixir: "~> 1.0",
+      description: "Encoder and decoder for the bencode format",
+      package: package(),
+      deps: deps()
+    ]
   end
 
   def application do
@@ -15,14 +17,18 @@ defmodule Bencodex.Mixfile do
   end
 
   defp deps do
-    [{:earmark, "~> 0.1", only: :dev},
-     {:ex_doc, "~> 0.6", only: :dev}]
+    [
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.6", only: :dev}
+    ]
   end
 
   defp package do
-    [contributors: ["Patrick Gombert"],
-     files: ["lib", "mix.exs", "README*", "LICENSE*"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/patrickgombert/bencodex"}]
+    [
+      contributors: ["Patrick Gombert"],
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/patrickgombert/bencodex"}
+    ]
   end
 end
